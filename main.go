f8e57298052e4791d13e2c49b0d018c0ba9984c5
egorfdv <egorfdvru@gmail.com>
2025-03-24 19:54:13 +0300

    Basic functionality has been created

--- main.go
+++ main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Start project")
+	transactions := []float64{}
+	for {
+		transaction := scanTransaction()
+		if transaction == 0 {
+			break
+		}
+		transactions = append(transactions, transaction)
+	}
+	fmt.Println(transactions)
+}
+
+func scanTransaction() float64 {
+	var transaction float64
+	fmt.Print("Enter transaction: ")
+	fmt.Scan(&transaction)
+	return transaction
 }
